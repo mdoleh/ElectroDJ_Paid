1f243b367374f3d1588531c4bff8b8445cb9eaf1
Mohammad Doleh <mdoleh@kent.edu>
2014-08-27 21:54:43 -0400

    Initial Commit with Working Code

---
+++ src/com/doleh/ElectroDJ_Paid/LaunchActivity.java
@@ -0,0 +1,28 @@
+package com.doleh.ElectroDJ_Paid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import com.doleh.Jukebox.MainActivity;
+
+public class LaunchActivity extends Activity
+{
+    private static final int SPLASH_TIMEOUT = 3000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(LaunchActivity.this, MainActivity.class);
+                i.putExtra(MainActivity.SHOW_ADS, false);
+                startActivity(i);
+            }
+        }, SPLASH_TIMEOUT);
+    }
+}
