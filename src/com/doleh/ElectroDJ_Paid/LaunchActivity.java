d376ef76d72bc06b19a95bc2892289018de2a726
Mohammad Doleh <mdoleh@kent.edu>
2014-09-27 19:20:08 -0400

    Splash screen only shows once

--- src/com/doleh/ElectroDJ_Paid/LaunchActivity.java
+++ src/com/doleh/ElectroDJ_Paid/LaunchActivity.java
@@ -15,19 +15,34 @@ public class LaunchActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(LaunchActivity.this, MainActivity.class);
-                i.putExtra(Config.SHOULD_SHOW_ADS_KEY, false);
-                i.putExtra(Config.APP_PNAME_KEY, getPackageName());
-                i.putExtra(Config.APP_TITLE_KEY, getString(R.string.app_name));
-                i.putExtra(Config.APP_PAID_KEY, true);
-                i.putExtra(Config.MAX_MESSAGE_COUNT_KEY, 10);
-                startActivity(i);
-            }
-        }, SPLASH_TIMEOUT);
+        if (Config.SHOW_SPLASH_SCREEN)
+        {
+            setContentView(R.layout.splash_screen);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = createIntent();
+                    startActivity(i);
+                }
+            }, SPLASH_TIMEOUT);
+        }
+        else
+        {
+            Intent i = createIntent();
+            startActivity(i);
+        }
+    }
+
+    private Intent createIntent()
+    {
+        Intent i = new Intent(LaunchActivity.this, MainActivity.class);
+        i.putExtra(Config.SHOULD_SHOW_ADS_KEY, false);
+        i.putExtra(Config.APP_PNAME_KEY, getPackageName());
+        i.putExtra(Config.APP_TITLE_KEY, getString(R.string.app_name));
+        i.putExtra(Config.APP_PAID_KEY, true);
+        i.putExtra(Config.MAX_MESSAGE_COUNT_KEY, 10);
+        return i;
     }
 }
