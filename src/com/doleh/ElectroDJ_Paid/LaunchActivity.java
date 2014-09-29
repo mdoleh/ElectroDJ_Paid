21c479ddcf5b4d35a6f892f5c59558d386464eeb
Mohammad Doleh <mdoleh@kent.edu>
2014-09-28 21:32:28 -0400

    Moved Config Namespacing

--- src/com/doleh/ElectroDJ_Paid/LaunchActivity.java
+++ src/com/doleh/ElectroDJ_Paid/LaunchActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import com.doleh.Jukebox.Config;
+import com.doleh.Jukebox.Static.Config;
 import com.doleh.Jukebox.MainActivity;
 
 public class LaunchActivity extends Activity
